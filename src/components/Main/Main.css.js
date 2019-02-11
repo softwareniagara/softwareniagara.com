6e39f38cab8b005c725f9ca039a92317e31a12ad
Nickolas Kenyeres <nickolas@knicklabs.com>
2019-02-11 09:32:36 -0500

    Fix spacing of footer

--- src/components/Main/Main.css.js
+++ src/components/Main/Main.css.js
@@ -3,10 +3,10 @@ import styled from 'styled-components'
 import theme from '../../util/theme'
 
 export default styled.main`
-  padding: 3rem 2rem;
+  padding: 3rem 2rem 1rem 2rem;
 
   @media screen and (max-width: ${theme.base.screen.sm}) {
-    padding: 2rem 1rem;
+    padding: 2rem 1rem 1rem 1rem;
   }
 
   @media screen and (max-width: ${theme.base.screen.xs}) {
