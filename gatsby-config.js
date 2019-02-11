0762f98e8a03c192b2c68b9ceec72fe4edae233c
Nickolas Kenyeres <nickolas@knicklabs.com>
2019-02-11 01:29:39 -0500

    Remove start_url override

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,6 @@ module.exports = {
         theme_color: '#94C1FF',
         display: 'standalone',
         orientation: 'any',
-        start_url: '?homescreen=1',
         version: '0.0.1',
         icons: {
           android: true,
